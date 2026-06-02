8a43bfcd1cc8184ed0db890f07e60e3cdb4fb12d
Sauraf Kumar <saurafsps7631@gmail.com>
2026-06-02 19:35:27 +0530

    Day 3

---
+++ Python Journey/Day3 Tasks.py
@@ -0,0 +1,27 @@
+# Task 1
+'''lst = [10,20,30,40,50]
+lst.append(30)
+print(lst)
+lst.pop()
+print(lst)'''
+# =======================================
+# Task 2
+'''details = {"name":"Saurav","age":20,"Marks":90,"Course":"BCA"}
+print(details)
+details["Marks"] = 75
+print(details)
+print(details.keys())
+print(details.values())'''
+# ================================================
+# Task 3
+'''st1 = {2,3,4,5,6,7}
+st2 = {3,4,5,9,10,11}
+union_set = st1.union(st2)
+
+intersection = st1.intersection(st2)
+print(st1)
+print(st2)
+print(f"After union {union_set}")
+print(f"After intersection{intersection}")'''
+# =====================================================
+# Task 4
