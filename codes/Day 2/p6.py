a364d2ba1b5e2b622bdbabeafd0d66cafc5b4fdc
Sauraf Kumar <saurafsps7631@gmail.com>
2026-07-04 22:25:39 +0530

    DAY 2

---
+++ codes/Day 2/p6.py
@@ -0,0 +1,19 @@
+# is_student = True
+# print(is_student)  # Output: True
+# print(not is_student)  # Output: False
+
+
+
+
+# num = 10
+# result = float(num)
+# print(result)
+# print(type(result))
+
+
+
+number = "123"
+print(type(number))
+result = int(number)
+print(result)
+print(type(result))
